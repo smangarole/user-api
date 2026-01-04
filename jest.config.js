4dc811dd64f09224313a96501ee8029fadb77ad3
Siddhartha Mangarole <siddhartha@speechify.com>
2026-01-04 16:44:37 +0530

    Reporting

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,35 @@ module.exports = {
   testMatch: ["**/test/**/*.test.js"],
   testEnvironment: "node",
   verbose: true,
-  testTimeout: 15000
+  testTimeout: 15000,
+
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "reports/junit",
+        outputName: "jest-results.xml",
+      },
+    ],
+    [
+      "./node_modules/jest-html-reporter",
+      {
+        pageTitle: "API Project Test Report",
+        outputPath: "reports/html/jest-report.html",
+        includeFailureMsg: true,
+        includeConsoleLog: true,
+      },
+    ],
+  ],
+
+  collectCoverageFrom: [
+    "server.js",
+    "routes/**/*.js",
+    "lib/**/*.js",
+    "!**/node_modules/**",
+    "!**/test/**",
+  ],
+  coverageDirectory: "coverage",
 };
 
